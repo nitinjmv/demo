d49b47440e017df28c1d43410fb5b9fd076dbf9d
nitinjmv <nitinjmv@gmail.com>
2023-12-07 10:51:23 +0000

    test

---
+++ src/main/java/com/example/demo/Application.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
+
+@RestController
+@RequestMapping("test")
+class TestController {
+
+	@GetMapping
+	public ResponseEntity<String> test(){
+		return ResponseEntity.ok("Hello..");
+	}
+}
